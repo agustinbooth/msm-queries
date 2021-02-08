73b616d026cd0b518a15092778daec943e9a2ffe
agustinvd10 <avalenzu@chicagobooth.edu>
2021-02-08 17:41:40 +0000

    A lot

---
+++ app/controllers/directors_controller.rb
@@ -0,0 +1,24 @@
+class DirectorsController < ApplicationController
+
+  def index
+    @list_of_directors = Director.all
+
+    render({ :template => "directors_template/index.html.erb"})
+  end
+
+  def wisest
+    @oldest = Director.where.not({:dob => nil}).order({:dob => :asc}).at(0)
+    render({ :template => "directors_template/eldest.html.erb"})
+  end
+
+    def director_detail
+
+      the_id = params.fetch("an_id")
+
+     @the_director = Director.where({ :id => the_id }).at(0)
+      @filmography = Movie.where ({ :director_id => the_id })
+
+    render({ :template => "directors_template/show.html.erb"})
+  end
+
+end
